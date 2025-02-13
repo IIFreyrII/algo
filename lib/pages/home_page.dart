37631ee6cef980e8e3d75ef0b0dc7727366234c7
Bryan <freyrecanbryan@gmail.com>
2025-02-13 13:32:25 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,7 +17,48 @@ class _HomepageState extends State<Homepage> {
           Icons.menu,
           color: Colors.grey[800],
         ),
-        actions: [Icon(Icons.person)],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(
+              Icons.person),
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          //1.Texto principal
+          Padding(
+            padding: EdgeInsets.all(
+              24.0),
+            child: Row(
+              children: [
+                Text(
+                  "I want to ",
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+                Text(
+                  "Eat",
+                  style: TextStyle(
+                    //Tamaño de letra
+                    fontSize: 32,
+                    //Negritas
+                    fontWeight: FontWeight.bold,
+                    //Subrayado
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ],
+            ),
+          )
+          //2. Pestañas (TabBar)
+
+          //3. Contenido de las pestañas (TabBarView)
+
+          //4. Carrito (Cart)
+        ],
       ),
     );
   }
